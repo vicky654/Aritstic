650eaeebf9e91a4a35f4ad7a4aa4c89142189f22
DevVickyVerma <vicky.credentia@gmail.com>
2023-05-28 01:51:18 +0530

    add condition for time

--- src/App.js
+++ src/App.js
@@ -3,21 +3,46 @@ import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ThemeContext from './contexts/ThemeContext';
 import LandingPage from './pages/LandingPage';
-import RegisterPage from './pages/RegisterPage'; // Import the RegisterPage component
+import RegisterPage from './pages/RegisterPage';
+
 import GlobalStyles from './styles/GlobalStyles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const AlternateLandingPage = () => <div>Alternate Landing Page</div>;
+const AlternateRegisterPage = () => <div>Alternate Register Page</div>;
+
 function App() {
   const { theme } = useContext(ThemeContext);
+  const getCurrentUniverseTime = () => new Date();
+  const universeTime = getCurrentUniverseTime();
+  const desiredTimeStart = new Date();
+  desiredTimeStart.setHours(1, 40, 0); // Set desired start time to 1:40 AM
+  const desiredTimeEnd = new Date();
+  desiredTimeEnd.setHours(2, 0, 0); // Set desired end time to 2:00 AM
+  const shouldShowLandingPage =
+    universeTime >= desiredTimeStart && universeTime <= desiredTimeEnd;
 
   return (
     <ThemeProvider theme={{ theme }}>
       <GlobalStyles />
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/register" element={<RegisterPage />} />{' '}
-          {/* Add the register page route */}
+          <Route
+            path="/"
+            element={
+              shouldShowLandingPage ? <LandingPage /> : <AlternateLandingPage />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              shouldShowLandingPage ? (
+                <RegisterPage />
+              ) : (
+                <AlternateRegisterPage />
+              )
+            }
+          />
         </Routes>
       </Router>
     </ThemeProvider>
